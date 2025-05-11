1497f292c8011870e90c1f39da41df66d38e1797
Sharief9381-tech <sharief938130@gmail.com>
2025-05-11 12:06:38 +0530

    Create report_generator.py

---
+++ report_generator.py
@@ -0,0 +1,9 @@
+# shadowmap
+
+import json
+from .config import OUTPUT_FILE
+
+def generate_report(data):
+    with open(OUTPUT_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+    print(f"[+] Report generated: {OUTPUT_FILE}")
