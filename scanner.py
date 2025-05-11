5be608deb0195c6d9c321de19044ff2ae5233b35
Sharief9381-tech <sharief938130@gmail.com>
2025-05-11 12:05:21 +0530

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,21 @@
+# shadowmap/scanner.py
+
+import nmap
+
+def scan_network(network_range):
+    scanner = nmap.PortScanner()
+    print(f"[+] Scanning network: {network_range}")
+    scanner.scan(hosts=network_range, arguments='-sS -T4')
+    results = []
+
+    for host in scanner.all_hosts():
+        if scanner[host].state() == 'up':
+            open_ports = []
+            for proto in scanner[host].all_protocols():
+                ports = scanner[host][proto].keys()
+                for port in ports:
+                    service = scanner[host][proto][port]['name']
+                    open_ports.append({'port': port, 'service': service})
+            results.append({'ip': host, 'ports': open_ports})
+
+    return results
