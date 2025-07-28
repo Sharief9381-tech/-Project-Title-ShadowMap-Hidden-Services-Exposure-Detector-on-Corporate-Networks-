5810974efde5b45828c38c131a9dfaf931843dc1
Shaik Sharief <sharief938130@gmail.com>
2025-07-28 10:46:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from scanner import scan_network
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "ShadowMap is Live 🎯"
+def index():
+    return render_template('index.html')
+
+@app.route('/scan', methods=['POST'])
+def scan():
+    target = request.form['target']
+    ports = request.form['ports']
+    result = scan_network(target, ports)
+    return render_template('index.html', result=result)
